0d9361ee07198f3063ae956d9a320c213ae700bf
ssappenf <ssappenf@mit.edu>
2020-12-03 11:38:22 -0500

    get rid of the green

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: #90EE90;
+  background-color:white;
 }
 .success-message {
   color: purple;
