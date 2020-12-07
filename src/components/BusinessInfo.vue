ffa39781034794fb1c917d39a64f84246f2a4693
ssappenf <ssappenf@mit.edu>
2020-12-07 12:26:28 -0500

    formatting and add setting link to alert

--- src/components/BusinessInfo.vue
+++ src/components/BusinessInfo.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <b-card
-      :title="business.name"
-      class="mb-5"
-    >
-
-      <div v-if = "editing">
+    <b-card :title="business.name" class="mb-5">
+      <div v-if="editing">
         <b-form-textarea
           id="textarea"
           v-model="description"
@@ -17,7 +13,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Business Type: "
-          label-for="businessType">
+          label-for="businessType"
+        >
           <b-form-select
             id="businessType"
             v-model.trim="type"
@@ -29,7 +26,8 @@
           label-cols-sm="4"
           label="Status: "
           label-for="status"
-          class="mt-1">
+          class="mt-1"
+        >
           <b-form-select
             id="status"
             v-model.trim="status"
@@ -39,10 +37,7 @@
 
         <u>Contact</u>
 
-        <b-form-group
-          label-cols-sm="4"
-          label="Address: "
-          label-for="address">
+        <b-form-group label-cols-sm="4" label="Address: " label-for="address">
           <b-form-input
             id="address"
             v-model="address"
@@ -50,10 +45,7 @@
           ></b-form-input>
         </b-form-group>
 
-        <b-form-group
-          label-cols-sm="4"
-          label="Phone: "
-          label-for="phone">
+        <b-form-group label-cols-sm="4" label="Phone: " label-for="phone">
           <b-form-input
             id="phone"
             v-model="phone"
@@ -61,10 +53,7 @@
           ></b-form-input>
         </b-form-group>
 
-        <b-form-group
-          label-cols-sm="4"
-          label="Email: "
-          label-for="email">
+        <b-form-group label-cols-sm="4" label="Email: " label-for="email">
           <b-form-input
             id="email"
             v-model="email"
@@ -76,7 +65,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Monday: "
-          label-for="mondayHours">
+          label-for="mondayHours"
+        >
           <b-form-input
             id="mondayHours"
             v-model="monday_hours"
@@ -87,7 +77,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Tuesday: "
-          label-for="tuesdayHours">
+          label-for="tuesdayHours"
+        >
           <b-form-input
             id="tuesdayHours"
             v-model="tuesday_hours"
@@ -98,7 +89,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Wednesday: "
-          label-for="wednesdayHours">
+          label-for="wednesdayHours"
+        >
           <b-form-input
             id="wednesdayHours"
             v-model="wednesday_hours"
@@ -109,7 +101,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Thursday: "
-          label-for="thursdayHours">
+          label-for="thursdayHours"
+        >
           <b-form-input
             id="thursdayHours"
             v-model="thursday_hours"
@@ -120,7 +113,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Friday: "
-          label-for="fridayHours">
+          label-for="fridayHours"
+        >
           <b-form-input
             id="fridayHours"
             v-model="friday_hours"
@@ -131,7 +125,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Saturday: "
-          label-for="saturdayHours">
+          label-for="saturdayHours"
+        >
           <b-form-input
             id="saturdayHours"
             v-model="saturday_hours"
@@ -142,7 +137,8 @@
         <b-form-group
           label-cols-sm="4"
           label="Sunday: "
-          label-for="sundayHours">
+          label-for="sundayHours"
+        >
           <b-form-input
             id="sundayHours"
             v-model="sunday_hours"
@@ -151,39 +147,42 @@
         </b-form-group>
 
         <div class="edit-info">
-
-        <b-row class="mt-4" >
-          <b-button class="ml-3" v-on:click="cancelChanges"> Cancel Changes</b-button>
-          <b-button class="ml-2" v-on:click="saveChanges">Save Changes</b-button>
-        </b-row>
+          <b-row class="mt-4">
+            <b-button class="ml-3" v-on:click="cancelChanges">
+              Cancel Changes</b-button
+            >
+            <b-button class="ml-2" v-on:click="saveChanges"
+              >Save Changes</b-button
+            >
+          </b-row>
         </div>
       </div>
 
       <div v-else>
-        <b-card-sub-title>{{ business.description }}</b-card-sub-title><br>
-        <p> Business Type: {{business.type}} </p>
-        <p> Status: {{business.status}} </p>
-        
-        
-        <p><u> Contact </u> </p>
-        Address: {{business.address}} <br>
-        Phone: {{business.phone}} <br>
-        Email: {{business.email}} <br><br>
+        <b-card-sub-title>{{ business.description }}</b-card-sub-title
+        ><br />
+        <p>Business Type: {{ business.type }}</p>
+        <p>Status: {{ business.status }}</p>
+
+        <p><u> Contact </u></p>
+        Address: {{ business.address }} <br />
+        Phone: {{ business.phone }} <br />
+        Email: {{ business.email }} <br /><br />
 
         <p><u> Hours </u></p>
-        Monday: {{business.monday_hours}} <br>
-        Tuesday: {{business.tuesday_hours}} <br>
-        Wednesday: {{business.wednesday_hours}} <br>
-        Thursday: {{business.thursday_hours}} <br>
-        Friday: {{business.friday_hours}}<br>
-        Saturday: {{business.saturday_hours}} <br>
-        Sunday: {{business.sunday_hours}} <br>
-        <div v-if="correctBusiness"> 
-          <b-button class="mt-4" v-on:click="editBusinessInfo">Edit business info</b-button>
+        Monday: {{ business.monday_hours }} <br />
+        Tuesday: {{ business.tuesday_hours }} <br />
+        Wednesday: {{ business.wednesday_hours }} <br />
+        Thursday: {{ business.thursday_hours }} <br />
+        Friday: {{ business.friday_hours }}<br />
+        Saturday: {{ business.saturday_hours }} <br />
+        Sunday: {{ business.sunday_hours }} <br />
+        <div v-if="correctBusiness">
+          <b-button class="mt-4" v-on:click="editBusinessInfo"
+            >Edit business info</b-button
+          >
         </div>
-        
       </div>
-
     </b-card>
   </div>
 </template>
@@ -206,32 +205,33 @@ export default {
       type: "",
       description: "",
       address: "",
-      monday_hours:"",
-      tuesday_hours:"",
-      wednesday_hours:"",
-      thursday_hours:"",
-      friday_hours:"",
-      saturday_hours:"",
-      sunday_hours:"",
+      monday_hours: "",
+      tuesday_hours: "",
+      wednesday_hours: "",
+      thursday_hours: "",
+      friday_hours: "",
+      saturday_hours: "",
+      sunday_hours: "",
       possibleStatuses: [],
       possibleTypes: [],
-      correctBusiness: false
+      correctBusiness: false,
     };
   },
   props: {
-    businessID: Number
+    businessID: Number,
   },
 
-  created: function() {
+  created: function () {
     this.loadStatuses();
     this.loadBusinessTypes();
     this.getBusiness();
-    this.correctBusiness=this.businessID.toString()===this.$cookie.get('business-auth');
+    this.correctBusiness =
+      this.businessID.toString() === this.$cookie.get("business-auth");
   },
 
   methods: {
-    getBusiness: function() {
-      axios.get(`/api/business/id/${this.businessID}`).then(response => {
+    getBusiness: function () {
+      axios.get(`/api/business/id/${this.businessID}`).then((response) => {
         this.business = response.data;
         this.name = this.business.name;
         this.password = this.business.password;
@@ -248,16 +248,13 @@ export default {
         this.friday_hours = this.business.friday_hours;
         this.saturday_hours = this.business.saturday_hours;
         this.sunday_hours = this.business.sunday_hours;
-        }); 
+      });
     },
-    editBusinessInfo: function(){
-      
-        this.editing = true;
-      
-      
+    editBusinessInfo: function () {
+      this.editing = true;
     },
-    
-    cancelChanges: function(){
+
+    cancelChanges: function () {
       this.editing = false;
       this.name = this.business.name;
       this.password = this.business.password;
@@ -275,14 +272,19 @@ export default {
       this.saturday_hours = this.business.saturday_hours;
       this.sunday_hours = this.business.sunday_hours;
     },
-    saveChanges: function(){
+    saveChanges: function () {
       let req = this.$data;
-      axios.put(`/api/business/id/${this.businessID}`, req).then( () => {
-        this.newAddress = "";
-        this.newPhone = "";
-        this.editing = false;
-        this.getBusiness();
-      }).catch(err => {eventBus.$emit("update-business-error", err);})
+      axios
+        .put(`/api/business/id/${this.businessID}`, req)
+        .then(() => {
+          this.newAddress = "";
+          this.newPhone = "";
+          this.editing = false;
+          this.getBusiness();
+        })
+        .catch((err) => {
+          eventBus.$emit("update-business-error", err);
+        });
     },
     loadStatuses: function () {
       axios.get("/api/business/statuses").then((response) => {
@@ -294,6 +296,6 @@ export default {
         this.possibleTypes = response.data;
       });
     },
-  }
+  },
 };
 </script>
